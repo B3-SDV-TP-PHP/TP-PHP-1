02d85e5283f8b1b2ca1a54c96c9caad6047854c2
victor haettinger <elie.lajoinie@supdevinci-edu.fr>
2024-11-07 14:09:29 +0100

    Ajout de base

---
+++ gestion_contacts/index.php
@@ -0,0 +1,12 @@
+<?php 
+require "header.php";
+require "footer.php";
+
+echo "<form action='index.php' method='post'>
+        <input type='text' name='name'>
+        <input type='text' name='name'>
+        <input type='text' name='name'>
+
+        <input type='submit' value='Submit'>
+    </form>";
+?>
